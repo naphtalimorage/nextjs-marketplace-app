1bca4f8cc8bcd4667348246fb4e620000a638940
Naphtali Morage <naphtalimorage56@example.com>
2024-09-26 19:48:02 +0300

    addded selects component and a separator component

--- src/app/(main)/buymotorbike/page.tsx
+++ src/app/(main)/buymotorbike/page.tsx
@@ -1,5 +1,27 @@
+import SearchBar from "@/components/ui/SearchBar";
+import { Button } from "@/components/ui/button";
+
 export default function Buybike() {
-    return(
-        <h1>Buy Bike Page</h1>
-    );
+  return (
+    <div className="h-screen bg-card flex flex-col items-center ">
+      <div className=" mt-10">
+        <h1 className="flex justify-center text-3xl font-bold text-primary">
+          Let&#39;s Find Your Dream Bike
+        </h1>
+        <p className="flex justify-center text-gray-400 text-sm">
+          We recommend the very used and newest bikes today, and also a friendly
+          price for you
+        </p>
+      </div>
+      <div>
+        <div className="flex flex-row gap-1 mb-2 mt-12">
+          <Button className="text-black font-semibold">New Bike</Button>
+          <Button className="text-black font-semibold">Used Bike</Button>
+        </div>
+        <div>
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
 }
